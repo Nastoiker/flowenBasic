0b1d6ae8af12c853798e2b50cc06ea2f7ecd3dc8
Nastoiker <damirdambinov@gmail.com>
2023-01-14 22:41:19 +0400

    edit prisma correct brand

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,13 +1,46 @@
 import {Link} from "react-router-dom";
-import {useAppSelector} from "../../store";
+import {useAppDispatch, useAppSelector} from "../../store";
+import {useState} from "react";
+import {getSecondCategory} from "../../store/SecondCategory.slice";
+interface IMenu {
+    name: string,
+    children?: IMenu[];
+}
 
 export const Menu = ():JSX.Element => {
-    const categoryes = useAppSelector(state => state.firstCategory.category);
+    const category = useAppSelector(state => state.firstCategory.category);
+    const dispatch = useAppDispatch();
+    const currentClicked = useState<string>('');
+    // let menu:IMenu[] = [];
+    // for(let i = 0; i < category.length; i++ ) {
+    //     let secondMenu:IMenu[] = [];
+    //     secondCategory.forEach( s => {
+    //         if(s.firstLevelId === category[i].id) {
+    //             secondMenu.push({name: s.name})
+    //         };
+    //     });
+    //     let secondEdit = second.map( s => {
+    //         delete s.id
+    //     })
+    //     if(second) {
+    //         menu[i] = {name: category[i].name, children: second,  },};
+    //     } else {
+    //         menu[i] = {name: category[i].name};
+    //     }
+    // }
+
     return (<nav>
-        categoryes
+        {category.map( c =>  {
+                dispatch(getSecondCategory(c.id));
+                const secondCategory = useAppSelector(state => state.secondCategory.category);
+                return (
+            <div key ={ c.id}>{c.name}{}</div>
+            )
+        }
+        )}
         {/*<Link to={''} ></Link>*/}
         {/*<Link to={''} ></Link>*/}
         {/*<Link to={''} ></Link>*/}
         {/*<Link to={''} ></Link>*/}
-    </nav>)
+    </nav>);
 }
