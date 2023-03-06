dc9a4272a1dac7d0a703a2e99154dae63384c70d
Tish61ka <yuriitish@mail.ru>
2023-03-06 11:45:49 +0400

    search

--- frontend/src/store/slices/search.slice.ts
+++ frontend/src/store/slices/search.slice.ts
@@ -1,20 +1,26 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {userSlice} from "./user.slice";
+import { SmartPhone } from "../../../interfaces/product.interfaces";
 interface SerchState {
     search: string;
     filters: string[];
+    founded: SmartPhone[];
     currentBrand: string;
     currentSecondCategory: string;
-    isLoading: boolean;
+    isFound: boolean;
 }
 const initialState:SerchState = {
     search: '',
-    isLoading: true,
+    isFound: true,
 };
 const searchSlice = createSlice({
     name: 'phones',
     initialState,
     reducers: {
+        search: (state, { payload}) => {
+          state.founded = payload.phones.name.includes(payload.name);
+          if(state.search.)
+        },
         setSearch: (state, action) => {
             state.search = action.payload
         },
