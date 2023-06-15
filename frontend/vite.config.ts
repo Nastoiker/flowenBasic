912a53deaa486df04bc6f197b55431c024a3dd83
Nastoiker <damirdambinov@gmail.com>
2023-06-15 21:29:51 +0400

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,14 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: {
+   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
+     // Thanks @sergiomoura for the window fix
     // add the next lines if you're using windows and hot reload doesn't work
     watch: {
-      usePolling: true
-    },
+       usePolling: true
+     },
     cors: false,
-  }
+   }
 });
